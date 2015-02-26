73f0ec46c40645814110e1c8e90cbc4873309c9e
YOLOswagger6669 <samosley@gmail.com>
2015-02-26 08:46:20 -0700

    Update IntArrayWorker.java

--- pixLab/classes/IntArrayWorker.java
+++ pixLab/classes/IntArrayWorker.java
@@ -98,5 +98,36 @@ public class IntArrayWorker
       }
     }
   }
- 
+  public int getCount(int x) {
+    int count=0;
+    int numCols = matrix[0].length;
+    for (int row = 0; row < matrix.length; row++)
+    {
+      for (int col = 0; col < numCols; col++)
+      {
+        if(matrix[row][col]==x)
+          count++;
+      }
+    }
+    return count;
+  }
+  public int getLargest() {
+    int largest=0;
+    for (int row = 0; row < matrix.length; row++)
+    {
+      for (int col = 0; col < matrix[0].length; col++)
+      {
+        if(matrix[row][col]>largest)
+          largest=matrix[row][col];
+      }
+    }
+    return largest;
+  }
+  public int getColTotal(int col) {
+    int total=0;
+    for(int i=0; i<matrix.length; i++) {
+      total+=matrix[i][col];
+    }
+    return total;
+  }
 }
